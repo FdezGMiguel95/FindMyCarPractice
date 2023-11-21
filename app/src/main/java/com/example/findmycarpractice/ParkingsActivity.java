6fc3fc65af9d6e6755e31ed96d981d87de047b97
miguel <fdezguerrerom@gmail.com>
2023-11-21 18:26:54 +0100

    Me ha venido mi padre a molestar -.-

--- app/src/main/java/com/example/findmycarpractice/ParkingsActivity.java
+++ app/src/main/java/com/example/findmycarpractice/ParkingsActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,13 +13,14 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class ParkingsActivity extends AppCompatActivity {
-    private Button btnAdd;
+    private Button btnGoAdd, btnGoMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parkings);
         ArrayList<Aparcamiento> parkings = new ArrayList<>();
-        btnAdd = (Button) findViewById(R.id.btnAddParking);
+        btnGoAdd = (Button) findViewById(R.id.btnAddParking);
+        btnGoMap = (Button) findViewById(R.id.btnGoToMap);
         parkings.add(new Aparcamiento("Madrid", 40.4165000f ,-3.7025600f));
         parkings.add(new Aparcamiento("Barcelona", 41.38879f , 2.15899f));
 
@@ -28,10 +30,17 @@ public class ParkingsActivity extends AppCompatActivity {
         rView.setLayoutManager(new LinearLayoutManager(this));
         rView.setAdapter(pointsAdapter);
 
-        btnAdd.setOnClickListener(new View.OnClickListener() {
+        btnGoMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent seeOnMap = new Intent(ParkingsActivity.this, MapActivity.class);
+            }
+        });
+        btnGoAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addAparcamiento = new Intent(ParkingsActivity.this, CreateParkingActivity.class);
+                startActivity(addAparcamiento);
             }
         });
     }
